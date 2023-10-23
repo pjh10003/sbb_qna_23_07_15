67f55e2ab13c44d517d93ade98711b1ea334f655
pjh10003@nate.com <pjh10003@nate.com>
2023-10-23 11:22:14 +0900

    ãResponseBody add

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -2,12 +2,14 @@ package com.exam.sbb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
   @RequestMapping("/sbb")
-  public void index()
+  @ResponseBody
+  public String index()
   {
-    System.out.println("첫 시작");
+    return "안녕하세요";
   }
 }
